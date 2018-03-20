c2e0d7d34c25f5c2eb87dce1694a8054087dfcc0
Satinderjit Singh <satinderjit01@gmail.com>
2018-03-20 06:28:04 -0700

    first commit

---
+++ kuka_arm/scripts/IK_server.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2017 Udacity Inc.
+#
+# This file is part of Robotic Arm: Pick and Place project for Udacity
+# Robotics nano-degree program
+#
+# All Rights Reserved.
+
+# Author: Harsh Pandya
+
+# import modules
+import rospy
+import tf
+from kuka_arm.srv import *
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from geometry_msgs.msg import Pose
+from mpmath import *
+from sympy import *
+
+
+def handle_calculate_IK(req):
+    rospy.loginfo("Received %s eef-poses from the plan" % len(req.poses))
+    if len(req.poses) < 1:
+        print "No valid poses received"
+        return -1
+    else:
+
+        ### Your FK code here
+        # Create symbols
+        q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
+        d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
+        a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+        alp0, alp1, alp2, alp3, alp4, alp5, alp6 = symbols('alp0:7')
+
+	
+	
+	# Create Modified DH parameters
+        s = {alp0:     0, a0:       0, d1:   0.75, q1: q1,
+             alp1: -pi/2, a1:    0.35, d2:      0, q2: q2-pi/2,
+             alp2:     0, a2:    1.25, d3:      0, q3: q3,
+             alp3: -pi/2, a3: -0.0536, d4: 1.5014, q4: q4,
+             alp4:  pi/2, a4:       0, d5:      0, q5: q5,
+             alp5: -pi/2, a5:       0, d6:      0, q6: q6,
+	     alp6:     0, a6:       0, d7:  0.303, q7: 0}
+
+	
+	# Define Modified DH Transformation matrix
+        def TF_Matrix(alp, a, d, q):
+             TF = Matrix([[         cos(q),         -sin(q),         0,           a],
+                          [sin(q)*cos(alp), cos(q)*cos(alp), -sin(alp), -sin(alp)*d],
+                          [sin(q)*sin(alp), cos(q)*sin(alp),  cos(alp),  cos(alp)*d],
+                          [              0,               0,         0,           1]])
+             return TF
+	
+	
+	# Create individual transformation matrices
+        T0_1 = TF_Matrix(alp0, a0, d1, q1).subs(s)
+        T1_2 = TF_Matrix(alp1, a1, d2, q2).subs(s)
+        T2_3 = TF_Matrix(alp2, a2, d3, q3).subs(s)
+        T3_4 = TF_Matrix(alp3, a3, d4, q4).subs(s)
+        T4_5 = TF_Matrix(alp4, a4, d5, q5).subs(s)
+        T5_6 = TF_Matrix(alp5, a5, d6, q6).subs(s)
+        T6_G = TF_Matrix(alp6, a6, d7, q7).subs(s)
+	
+	
+	# Extract rotation matrices from the transformation matrices
+        T0_G = simplify(T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_G)
+	
+	
+        ###
+        r, p, y = symbols('r p y')
+        ROT_x = Matrix([[1,      0,       0],
+                        [0, cos(r), -sin(r)],
+                        [0, sin(r),  cos(r)]])
+
+        ROT_y = Matrix([[cos(p),  0,  sin(p)],
+                        [0,       1,       0],
+                        [-sin(p), 0,  cos(p)]])
+
+        ROT_z = Matrix([[cos(y), -sin(y),  0],
+                        [sin(y),  cos(y),  0],
+                        [     0,       0,  1]])
+
+        ROT_G = simplify(ROT_z * ROT_y * ROT_x)
+
+        print "ROT_G: " ,ROT_G
+
+        ROT_error = ROT_z.subs(y, pi) * ROT_y.subs(p, -pi/2)
+
+        ROT_G = simplify(ROT_G * ROT_error)
+
+        R0_3 = T0_1[0:3,0:3] * T1_2[0:3,0:3] * T2_3[0:3,0:3]
+
+        # Initialize service response
+        joint_trajectory_list = []
+        for x in xrange(0, len(req.poses)):
+            # IK code starts here
+            joint_trajectory_point = JointTrajectoryPoint()
+
+	    # Extract end-effector position and orientation from request
+	    # px,py,pz = end-effector position
+	    # roll, pitch, yaw = end-effector orientation
+            px = req.poses[x].position.x
+            py = req.poses[x].position.y
+            pz = req.poses[x].position.z
+
+            (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
+                [req.poses[x].orientation.x, req.poses[x].orientation.y,
+                    req.poses[x].orientation.z, req.poses[x].orientation.w])
+
+            ### Your IK code here
+	    # Compensate for rotation discrepancy between DH parameters and Gazebo
+
+	   
+            ROT_G = ROT_G.subs({'r': roll, 'p': pitch, 'y': yaw}) 
+
+
+            #G = Matrix ([[px],
+            #             [py],
+            #             [pz]])
+
+            #WC = G - (0.303) * ROT_G[:,2]
+            wx = px - 0.303 * ROT_G[0,2]
+            wy = py - 0.303 * ROT_G[1,2]
+            wz = pz - 0.303 * ROT_G[2,2]
+	    #
+	    #
+	    # Calculate joint angles using Geometric IK method
+            theta1 = atan2(wy, wx)
+            side_a = 1.5014
+            r = sqrt(wx*wx+wy*wy) - 0.35 # a1: 0.35
+            side_b = sqrt((r*r) + pow(wz - 0.75, 2))
+            side_c = 1.25
+	    
+            angle_a = acos((side_b * side_b + side_c * side_c - side_a * side_a) / (2 * side_b * side_c))
+            angle_b = acos((side_a * side_a + side_c * side_c - side_b * side_b) / (2 * side_a * side_c))
+            angle_c = acos((side_a * side_a + side_b * side_b - side_c * side_c) / (2 * side_a * side_b))
+
+
+            theta2 = pi/2 - angle_a - atan2(wz - 0.75, r)
+            theta3 = pi/2 - (angle_b + 0.036) # 0.036 is for sag in link4
+
+            R0_3 = R0_3.evalf(subs={'q1':theta1, 'q2':theta2, 'q3':theta3})
+
+            R3_6 = R0_3.transpose() * ROT_G
+            
+            theta4 = atan2(R3_6[2,2], -R3_6[0,2])
+            theta5 = atan2(sqrt(R3_6[0,2]*R3_6[0,2] + R3_6[2,2]*R3_6[2,2]), R3_6[1,2])
+            theta6 = atan2(-R3_6[1,1], R3_6[1,0])
+            ###
+
+            # Populate response for the IK request
+            # In the next line replace theta1,theta2...,theta6 by your joint angle variables
+	    joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
+	    joint_trajectory_list.append(joint_trajectory_point)
+
+        rospy.loginfo("length of Joint Trajectory List: %s" % len(joint_trajectory_list))
+        return CalculateIKResponse(joint_trajectory_list)
+
+
+def IK_server():
+    # initialize node and declare calculate_ik service
+    rospy.init_node('IK_server')
+    s = rospy.Service('calculate_ik', CalculateIK, handle_calculate_IK)
+    print "Ready to receive an IK request"
+    rospy.spin()
+
+if __name__ == "__main__":
+    IK_server()
